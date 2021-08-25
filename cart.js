68f1a21afe2b0d7ceda52da8144652d23a062965
anwar_moazam <anwar.palimarwar@gmail.com>
2021-08-25 15:38:18 +0530

    Cart Setup

--- cart.js
+++ cart.js
@@ -1,81 +1,98 @@
-let list = document.querySelector(".item-container");
-let grossAmount = document.querySelectorAll(".cart-total");
-// let qty = document.querySelector(".amount");
-// let initialCount = 1;
+let list = document.querySelector('.item-container');
+let grossAmountDOM = document.querySelector('.cart-total');
+let grossQtyDOM = document.querySelector('.total-amount');
+let footerDOM = document.querySelector("footer");
 
-const cartItems = [
+let cartItems = [
 	{
 		id: 1,
 		name: 'Samsung Galaxy S8',
-        price: 399.99,
+		price: 399.99,
 		img: './images/item-1.jpeg',
-		qty: 1
+		qty: 1,
 	},
-    {
+	{
 		id: 2,
 		name: 'Google pixel',
-        price: 499.99,
+		price: 499.99,
 		img: './images/item-2.jpeg',
-		qty: 1
+		qty: 1,
 	},
 	{
 		id: 3,
 		name: 'Xiaomi Redmi Note 2',
-        price: 699.99,
+		price: 699.99,
 		img: './images/item-3.jpeg',
-		qty: 1
+		qty: 1,
 	},
 	{
 		id: 4,
 		name: 'Samsung Galaxy S7',
-        price: 599.99,
+		price: 599.99,
 		img: './images/item-4.jpeg',
-		qty: 1
-	}
+		qty: 1,
+	},
 ];
 
-function renderItem(item){
-    return`<div><article class="cart-item"><img src="${item.img}" alt="Samsung Galaxy S8"/><div><h4>${item.name}</h4><h4 class="item-price">${item.price}</h4><button class="remove-btn">remove</button></div><div><button onclick="increaseItem(this)" class="amount-btn">+</button><p class="amount">${item.qty}</p><button onclick="decreaseItem(this)" class="amount-btn">-</button></div></article></div>`;
+function renderItem(item) {
+	return `<div><article class="cart-item"><img src="${item.img}" alt="${item.name}"/><div><h4>${item.name}</h4><h4 class="item-price">${item.price}</h4><button onclick="removeItem(${item.id})" class="remove-btn">remove</button></div><div><button onclick="increaseItem(${item.id})" class="amount-btn">+</button><p class="amount">${item.qty}</p><button onclick="decreaseItem(${item.id})" class="amount-btn">-</button></div></article></div>`;
 }
 
-(function render() {
+function render() {
 	let products = '';
+	let grossQty = 0;
+	let grossAmount = 0;
 	for (let item of cartItems) {
-		products += renderItem(item);
-	}
-	list.innerHTML = products;
-})();
-
-function increaseItem(e){
-	let qty = document.querySelector(".amount");
-	let amount = document.querySelectorAll(".item-price");
-	for(let item of cartItems){
-		item.qty++;
-		cartItems.qty = item.qty;
-		qty.innerHTML = cartItems.qty;
-	}
-	showTotal();
-}
-
-function decreaseItem(e){
-	let qty = document.querySelector(".amount");
-	for(let item of cartItems){
-		if(item.qty>1){
-			item.qty--;
-			cartItems.qty = item.qty;
-			qty.innerHTML = cartItems.qty;
-		} else{
-			let last = document.querySelector(".cart-item")
-			last.parentNode.innerHTML = '';
+		if (item.qty > 0) {
+			products += renderItem(item);
+			grossQty += item.qty;
+			grossAmount += item.price * item.qty;
 		}
 	}
+	list.innerHTML = products;
+	grossAmountDOM.innerText = grossAmount.toFixed(2);
+	grossQtyDOM.innerText = grossQty;
+	if(grossQty===0){
+		footerDOM.remove();
+	}
+	return;
 }
 
-function showTotal(){
-	let sum = 0;
-	for(let item of cartItems){
-		sum += cartItems.qty * cartItems.price;
-		console.log(sum);
+function increaseItem(id) {
+	for (let item of cartItems) {
+		if (item.id === id) {
+			item.qty++;
+			break;
+		}
 	}
-	grossAmount[0].children[0].childNodes[1].innerHTML = sum;
+	render();
 }
+
+function decreaseItem(id) {
+	for (let item of cartItems) {
+		if (item.id === id) {
+			if (item.qty > 0) {
+				item.qty--;
+				break;
+			}
+		}
+	}
+	render();
+}
+
+function removeItem(id) {
+	for (let item of cartItems) {
+		if (item.id === id) {
+			item.qty = 0;
+			break;
+		}
+	}
+	render();
+}
+
+function clearCart() {
+	cartItems = [];
+	render();
+}
+
+render();
